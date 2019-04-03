43fc8f3928ad05e4bc10f4e0188a83f7ef30f94d
witek <witkups@gmail.com>
2019-04-03 21:31:34 +0200

    Fix degrees calculation

--- utils/angle_detector.py
+++ utils/angle_detector.py
@@ -23,7 +23,7 @@ class Angle:
 
     @staticmethod
     def calculate_angle_between(a, b, c):
-        ang = math.degrees(math.atan2(c[1] - b[1], c[0] - b[0]) - math.atan2(a[1] - b[1], a[0] - b[0]))
+        ang = math.degrees(math.atan2(c[0] - b[0], c[1] - b[1]) - math.atan2(a[0] - b[0], a[1] - b[1]))
         return ang + 360 if ang < 0 else ang
 
     @staticmethod
