a21e5b8647530df373fece50b4616927a5921602
Jakub Tomczak <jakub.tomczak@live.com>
2019-04-17 20:45:42 +0200

    Add type for linspace.

--- utils/plotting_helper.py
+++ utils/plotting_helper.py
@@ -7,8 +7,8 @@ def plot_line(ax, coord_1, coord_2, line_color):
     ax.plot(xx, yy, 'ro-', color=line_color)
 
 
-def interpolate_between_points(coord1: [float, float], coord2: [float, float], number_of_points: int) -> (
-        [float], [float]):
-    xx = np.linspace(coord1[1], coord2[1], number_of_points)
-    yy = np.linspace(coord1[0], coord2[0], number_of_points)
-    return xx, yy
+def interpolate_between_points(coord1: [float, float], coord2: [float, float], number_of_points: int,
+                               target_type=float) -> ([float], [float]):
+    xx = np.linspace(coord1[1], coord2[1], number_of_points, dtype=target_type)
+    yy = np.linspace(coord1[0], coord2[0], number_of_points, dtype=target_type)
+    return yy, xx
