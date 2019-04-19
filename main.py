1e5b1840f43d25749edf198a6b48cc7de9d67446
Jakub Tomczak <jakub.tomczak@live.com>
2019-04-19 08:35:16 +0200

    Switch between two methods for finding matching images.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import argparse
 
+from methods.vector_based_detector import find_matching_images
 from utils.angle_detector import get_ranking
-from utils.dataset_helper import load_dataset
+from utils.dataset_helper import load_dataset, Dataset
 from utils.debug_helper import print_debug_info
 from utils.debug_conf import *
 
@@ -14,14 +15,24 @@ def parse_args():
     return args.path, int(args.number_of_images)
 
 
-def run(path, number_of_images, debug=False, display_ranking=True):
-    dataset = load_dataset(path, number_of_images)
+def method_one(dataset: Dataset, debug: bool, display_ranking: bool):
     ranking = get_ranking(dataset)
     if display_ranking:
         for r in ranking:
             print(" ".join(str(i) for i in r))
     if debug:
         print_debug_info(dataset, ranking)
+
+
+def method_two(dataset: Dataset, debug: bool, display_ranking: bool):
+    find_matching_images(dataset)
+
+
+def run(path, number_of_images, debug=False, display_ranking=True):
+    dataset = load_dataset(path, number_of_images)
+
+    method = method_two
+    method(dataset, debug, display_ranking)
 
 
 if __name__ == "__main__":
